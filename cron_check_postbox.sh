12fbae809edf747ce8c651919d2c4e2c890fda59
MatthewMarmalade <55963631+MatthewMarmalade@users.noreply.github.com>
2026-01-12 15:16:02 -0800

    Update cron_check_postbox.sh
    Filling out complete paths

--- cron_check_postbox.sh
+++ cron_check_postbox.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-latest_comic=$(< config/latest_comic.txt)
+latest_comic=$(< softer/postbox/config/latest_comic.txt)
 next_comic=$(($latest_comic + 1))
 
-./mail_comic.sh $next_comic
+softer-postbox/mail_comic.sh $next_comic
 
 # if successful, update our latest comic count
 if [ $? -eq 0 ]; then
-	echo $next_comic > config/latest_comic.txt
+	echo $next_comic > softer-postbox/config/latest_comic.txt
 else
 	echo "Exit code $? does not equal 0"
 fi
