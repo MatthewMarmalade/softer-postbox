dedc68b7d4d04733866ccb4fa8a384af4d5d8370
MatthewMarmalade <55963631+MatthewMarmalade@users.noreply.github.com>
2026-01-12 15:18:04 -0800

    Update get_comic.sh
    Attempt to remove relative paths

--- get_comic.sh
+++ get_comic.sh
@@ -8,16 +8,16 @@ then
     exit 1
 fi
 
-curl -s -o "temporary_comic_page.html" "https://www.asofterworld.com/index.php?id=$1"
+curl -s -o "softer-postbox/temporary_comic_page.html" "https://www.asofterworld.com/index.php?id=$1"
 
 # extract the image link from the html
-comic_file_name=$(grep -o 'https://www.asofterworld.com/clean/.*\.jpg' temporary_comic_page.html)
+comic_file_name=$(grep -o 'https://www.asofterworld.com/clean/.*\.jpg' softer-postbox/temporary_comic_page.html)
 
 if [ $? -ne 0 ];
 then
-	echo "Failed to fetch comic $1, perhaps out of bounds?" >> error_log.txt
+	echo "Failed to fetch comic $1, perhaps out of bounds?" >> softer-postbox/error_log.txt
 	echo "Failed to fetch comic $1, perhaps out of bounds?"
-	rm temporary_comic_page.html
+	rm softer-postbox/temporary_comic_page.html
 	exit 2
 fi
 
@@ -25,8 +25,8 @@ fi
 comic_title_text=$(grep 'makeAlert' temporary_comic_page.html | grep -E -o "'.+'" | sed "s/'', //")
 
 # fetch the comic image itself and store it in a file
-curl -s -o "comic_$1.jpg" "$comic_file_name"
+curl -s -o "softer-postbox/comic_$1.jpg" "$comic_file_name"
 
 echo "$comic_title_text"
 
-rm temporary_comic_page.html
+rm softer-postbox/temporary_comic_page.html
